910205b2b69241335896a2260aee6c5b004d0eab
Carla <carlablockley1@gmail.com>
2025-08-05 13:55:39 +0000

    start of end points

--- src/main/java/com/restapi/restapi/controller/FinanceDataController.java
+++ src/main/java/com/restapi/restapi/controller/FinanceDataController.java
@@ -2,6 +2,7 @@ package com.restapi.restapi.controller;
 
 import com.restapi.restapi.dto.PasswordsDTO;
 import com.restapi.restapi.dto.QuoteDTO;
+import com.restapi.restapi.model.Passwords;
 import com.restapi.restapi.model.Quote;
 import com.restapi.restapi.service.FinanceDataService;
 import com.restapi.restapi.service.PasswordsService;
@@ -9,6 +10,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class FinanceDataController {
@@ -25,11 +28,33 @@ public class FinanceDataController {
         return ResponseEntity.ok(quoteDTO);
     }
 
+    @GetMapping("/quotes")
+    public ResponseEntity<List<QuoteDTO>> getQuotes(){
+        List<QuoteDTO> quotes = finService.getAllQuotes();
+
+        return quotes.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(quotes);
+    }
+
     @PostMapping("/quote")
     public ResponseEntity<String> postPasswords(@RequestParam String symbol){
         boolean success = finService.createQuote(symbol);
 
         return success ? ResponseEntity.ok("Successfully inserted " + symbol + " symbol!") :
                 ResponseEntity.badRequest().body("Inserted failed!");
+    }
+
+    @PutMapping("/quotes/{id}")
+    public ResponseEntity<String> putQuote(@PathVariable int id, @RequestBody QuoteDTO quoteDTO){
+        boolean success = finService.putPassword(passwordsDTO, id);
+
+        return success ? ResponseEntity.ok("Successfully done the update!") :
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body("No passwords with this ID!");
+    }
+
+    @DeleteMapping("/passwords/{id}")
+    public ResponseEntity<String> deletePasswords(@PathVariable int id){
+        boolean success = passwordsService.deletePassword(id);
+        return success ? ResponseEntity.ok("Successfully deleted") :
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body("No passwords with this ID!");
     }
 }
--- src/main/java/com/restapi/restapi/service/FinanceDataService.java
+++ src/main/java/com/restapi/restapi/service/FinanceDataService.java
@@ -1,12 +1,17 @@
 package com.restapi.restapi.service;
 
 import com.restapi.restapi.client.FinanceDataProvider;
+import com.restapi.restapi.dto.PasswordsDTO;
 import com.restapi.restapi.dto.QuoteDTO;
 import com.restapi.restapi.dto.external.finnhub.FinnhubQuoteRaw;
 import com.restapi.restapi.mapper.FinnhubQuoteMapper;
+import com.restapi.restapi.model.Passwords;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FinanceDataService {
@@ -27,7 +32,15 @@ public class FinanceDataService {
             return quoteMapper.map(raw, symbol);
         }
 
-        // POST
+    public List<QuoteDTO> getAllQuotes(){
+        List<QuoteDTO> quotes = jdbcTemplate.query("select * from Quote",
+                new Object[]{}, new BeanPropertyRowMapper<>(QuoteDTO.class));
+
+        return quotes;
+    }
+
+
+    // POST
         public boolean createQuote(String symbol){
             QuoteDTO quoteDTO = this.getQuote(symbol);
 
@@ -38,4 +51,34 @@ public class FinanceDataService {
                     quoteDTO.getLowPriceOfDay(), quoteDTO.getOpenPriceOfDay(), quoteDTO.getPrevClosePrice());
             return rows == 1;
         }
+
+        //PUT
+    public boolean putQuote(QuoteDTO quoteDTO, final String symbol){
+        QuoteDTO existing = getQuote(symbol);
+
+        if(existing == null){
+            return false;
+        }
+
+        if(quoteDTO.getCurrentPrice() != 0) existing.setCurrentPrice(quoteDTO.getCurrentPrice());
+        if(quoteDTO.getChanges() != 0) existing.setChanges(quoteDTO.getChanges());
+        if(quoteDTO.getPercentChange() != 0) existing.setPercentChange(quoteDTO.getCurrentPrice());
+        if(quoteDTO.getHighPriceOfDay() != 0) existing.setHighPriceOfDay(quoteDTO.getHighPriceOfDay());
+        if(quoteDTO.getLowPriceOfDay() != 0) existing.setLowPriceOfDay(quoteDTO.getLowPriceOfDay());
+        if(quoteDTO.getOpenPriceOfDay() != 0) existing.setOpenPriceOfDay(quoteDTO.getOpenPriceOfDay());
+        if(quoteDTO.getPrevClosePrice() != 0) existing.setPrevClosePrice(quoteDTO.getPrevClosePrice());
+
+        String sql = "UPDATE Quote set currentPrice = ?, changes = ?, percentChange = ?, url = ?, notes = ?, modificationdate = CURDATE() where symbol = ?";
+        int rows = jdbcTemplate.update(sql, existing.getName(), existing.getPassword(),
+                existing.getUrl(), existing.getNotes(), id);
+
+        return rows == 1;
+    }
+
+    //DELETE
+    public boolean deleteQuote(String symbol){
+        String sql = "DELETE FROM Quote where symbol = ?";
+        int rows = jdbcTemplate.update(sql, symbol);
+        return rows == 1;
+    }
 }
