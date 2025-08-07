e60ac96c54ca63f0370ae1a7b8a2af9adbfb2153
Kristof Toth <tothkristof017@gmail.com>
2025-08-07 11:28:39 +0000

    stock finnhub wrapper added

---
+++ src/main/java/com/restapi/restapi/model/Portfolio.java
@@ -0,0 +1,15 @@
+package com.restapi.restapi.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Portfolio {
+    private int id; // id
+    private String symbol; // Stock symbol
+    private double pricePerShare; // Price per share at purchase
+    private double shares; // Number of shares purchased
+    private double totalPrice; // Total price paid = pricePerShare * shares
+    private Date purchaseDate; // Purchase date
+}
