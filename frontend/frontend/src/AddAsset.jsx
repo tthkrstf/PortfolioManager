ca08f13f84b330734ec36b1eb340bfb1b8eec036
Harshiini Sridhar <harshiiniuk@gmail.com>
2025-08-08 15:18:50 +0000

    add asset done

--- frontend/frontend/src/AddAsset.jsx
+++ frontend/frontend/src/AddAsset.jsx
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import { AllCommunityModule, ModuleRegistry} from 'ag-grid-community';
 ModuleRegistry.registerModules([AllCommunityModule]);
 import {AgGridReact} from 'ag-grid-react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 
 function AddAsset(props) {
-    const handleChange = (event) => {
+    const handleChangeCompany = (event) => {
             setSelectedAssetCompany(event.target.value);
             setSelectedCompanyData(props.data[event.target.value]);
 
@@ -18,6 +21,15 @@ function AddAsset(props) {
     const [selectedAssetCompany, setSelectedAssetCompany] = useState("");
     const [selectedCompanyData, setSelectedCompanyData] = useState({});
     const [rowData, setRowData] = useState([props.rowData]);
+    const[inputValue, setInputValue] = useState('');
+    const [shares, setShares] = useState(0);
+
+    const handleInput = (event) => {
+        setInputValue(event.target.value);
+        };
+    const handleClick = () => {
+        setShares(inputValue);
+        };
 
     return (
         <div>
@@ -36,7 +48,7 @@ function AddAsset(props) {
                       label="Select company asset"
                       defaultValue={companyNames[0]}
                       value={selectedAssetCompany}
-                      onChange={handleChange}
+                      onChange={handleChangeCompany}
                     >
                     {companyNames.map((name) => (
                         <MenuItem key={name} value={name}>
@@ -46,6 +58,17 @@ function AddAsset(props) {
                     </TextField>
                     <div className="ag-theme-alpine" style={{width:"30rem", height: "20rem"}} >
                     <AgGridReact rowData={selectedCompanyData} columnDefs={props.colDefs} defaultColDef={{flex : 1, minWidth : 100, resizable: true}} />
+                     <div>
+                     <TextField
+                      required
+                      id="outlined-required"
+                      label="Number of shares"
+                      value={inputValue}
+                      onChange={handleInput}
+                    >
+                    </TextField>
+                    <Button variant="outlined" onClick={handleClick}>Buy</Button>
+                    </div>
                     </div>
                     </div>
                 </Box>
