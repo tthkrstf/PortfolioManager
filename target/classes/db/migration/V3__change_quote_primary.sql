e1f232c97f32bfc59603f2a4ddbce7b09fe38348
tthkrstf <tothkristof017@gmail.com>
2025-08-08 16:30:09 +0000

    Delete target directory

--- target/classes/db/migration/V3__change_quote_primary.sql
+++
@@ -1,9 +0,0 @@
-delete from quote; -- any changes for this table will be deleted to not cause any issues in chaning
-
-ALTER TABLE quote ADD UNIQUE (id);
-
-ALTER TABLE quote DROP PRIMARY KEY;
-
-ALTER TABLE quote DROP COLUMN id;
-
-ALTER TABLE quote ADD PRIMARY KEY (symbol, creation_date);
--- target/classes/db/migration/__notsure__V6__join_setup.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE portfolio
-ADD CONSTRAINT fk_portfolio_figi
-FOREIGN KEY (figi)
-REFERENCES stock(figi);
-
-ALTER TABLE portfolio
-ADD CONSTRAINT fk_portfolio_quote
-FOREIGN KEY (symbol, purchase_date)
-REFERENCES quote(symbol, creation_date);
